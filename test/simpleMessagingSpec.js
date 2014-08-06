fab6679df883ae7db8e38377b12691531e3e6202
sahil <sahil.kabra@gmail.com>
2014-08-06 13:41:20 +0530

    Using mocha and chai instead of jasmine

--- test/simpleMessagingSpec.js
+++ test/simpleMessagingSpec.js
@@ -1,29 +1,31 @@
+var expect = require('chai').expect;
+
 describe('The application event emitter', function() {
         it ('should return the same instance with multiple require calls for same path', function() {
-                var instance1 = require('app/simpleMessaging');
-                var instance2 = require('app/simpleMessaging');
-                expect(instance1).toEqual(instance2);
+                var instance1 = require('app/messenger');
+                var instance2 = require('app/messenger');
+                expect(instance1).to.equal(instance2);
         });
         it('events emitted should be global when module loaded using same path', function(done) {
-                var instance1 = require('app/simpleMessaging');
-                var instance2 = require('app/simpleMessaging');
+                var instance1 = require('app/messenger');
+                var instance2 = require('app/messenger');
                 var eventFired = false;
-                instance1.on('testEvent', function() {
+                instance1.once('testEvent', function() {
                         eventFired = true;
+                        expect(eventFired).to.equal(true);
                         done();
                 });
                 instance2.emit('testEvent');
-                expect(eventFired).toEqual(true);
         });
         it('events emitted are global even when module loaded using differing paths', function(done) {
-                var instance1 = require('app/simpleMessaging');
-                var instance2 = require('../src/simpleMessaging');
+                var instance1 = require('app/messenger');
+                var instance2 = require('../src/messenger');
                 var eventFired = false;
-                instance1.on('testEvent', function() {
+                instance1.once('testEvent', function() {
                         eventFired = true;
+                        expect(eventFired).to.be.true;
                         done();
                 });
                 instance2.emit('testEvent');
-                expect(eventFired).toEqual(true);
         });
 });
